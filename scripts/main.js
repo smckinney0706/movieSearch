34ff6c2ff870f0363d0c0eba6388e78bafb9d852
smckinney0706 <smckinney0706@gmail.com>
2017-04-17 20:25:11 -0500

    final commit. Changed format to Angular.

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,51 @@
 			
 			
 // 		});
-$("#button").click(function(){
+// $("#button").click(function(){
+// 			$("#content").html("");
+// 		$.getJSON("http://www.omdbapi.com/?s=" + $("#movieInput").val(), function(data){
+// 			// document.getElementById("contentBox1").innerHTML += data.Search.Title;
+// 			// document.getElementById("#movieInput").innerHTML = "";
+// 			for(var i = 0; i < data.Search.length; i++){
+// 				$("#content").append("<h1>" + data.Search[i].Title + "</h1>" + "<br><img src="+ data.Search[i].Poster +" />");
+// 			}
+			
+// 		});
+		
+// 	});
 
-		$.getJSON("http://www.omdbapi.com/?s=" + $("#movieInput").val(), function(data){
-			// document.getElementById("contentBox1").innerHTML += data.Search.Title;
-			// document.getElementById("#movieInput").innerHTML = "";
-			for(var i = 0; i < data.Search.length; i++){
-				$("#content").append("<h1>" + data.Search[i].Title + "</h1>" + "<br><img src="+ data.Search[i].Poster +" />");
-			}
+var app = angular.module('movieApp', []);
+app.controller('movieCtrl', function($scope, $http){
+
+	
+
+	$scope.showMovies = function(){
+		
+		$http.get("http://www.omdbapi.com/?s=" + $("#movieInput").val() + "&y=" + $("#yearInput").val() + "&type=" + $("#typeInput").val())
+		.then(function(response){
+			$scope.movies = response.data.Search;
 			
 		});
-		
-	});
+	};
+
+	
+
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
